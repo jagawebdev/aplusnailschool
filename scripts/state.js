87174879af91a734f512d8a4f0684e437d921f07
jaga <jagawebdev@gmail.com>
2016-08-17 05:07:08 +0000

    state page done

--- scripts/state.js
+++ scripts/state.js
@@ -13,3 +13,42 @@ $(document).ready(function() {
     });
 
 });
+
+        
+    $(".board-nav1").click(function() {
+        var currentBoard = $('.active-board');
+        var currentNav =$(".active-nav");
+        $(".board-nav1").addClass("active-nav");
+        $(".board1").addClass("active-board");
+        currentBoard.removeClass("active-board");
+        currentNav.removeClass("active-nav");
+    });
+    
+    
+    
+     $(".board-nav2").click(function() {
+         var currentBoard = $('.active-board');
+        var currentNav =$(".active-nav");
+        $(".board-nav2").addClass("active-nav");
+        $(".board2").addClass("active-board");
+        currentBoard.removeClass("active-board");
+        currentNav.removeClass("active-nav");
+    });
+    
+     $(".board-nav3").click(function() {
+         var currentBoard = $('.active-board');
+        var currentNav =$(".active-nav");
+        $(".board-nav3").addClass("active-nav");
+        $(".board3").addClass("active-board");
+        currentBoard.removeClass("active-board");
+        currentNav.removeClass("active-nav");
+    });
+    
+     $(".board-nav4").click(function() {
+         var currentBoard = $('.active-board');
+        var currentNav =$(".active-nav");
+        $(".board-nav4").addClass("active-nav");
+        $(".board4").addClass("active-board");
+        currentBoard.removeClass("active-board");
+        currentNav.removeClass("active-nav");
+    });
