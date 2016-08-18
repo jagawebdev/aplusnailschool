f53a88956e81958c70e27d8ec2e2686e753ae1c5
jaga <jagawebdev@gmail.com>
2016-08-18 23:12:29 +0000

    gallery pic add

---
+++ scripts/gallery.js
@@ -0,0 +1,38 @@
+/*global $ */
+/*global angular */
+
+$(document).ready(function() {
+  
+  
+  $('.hamburger').click( function(event){
+        event.stopPropagation();
+        $('.drop-menu').toggle();
+    });
+
+    $(document).click( function(){
+        $('.drop-menu').hide();
+    });
+
+});
+
+
+var app = angular.module("myApp", []);
+
+app.controller("MainController", 
+['$scope', function($scope) {
+  $scope.products = [
+    {cover: 'images/gallery/pic9.jpg',},
+    {cover: 'images/gallery/pic10.jpg',},
+    {cover: 'images/gallery/pic11.jpg',},
+    {cover: 'images/gallery/pic12.jpg',},
+    {cover: 'images/gallery/pic1.jpg',},
+    {cover: 'images/gallery/pic3.jpg',},
+    {cover: 'images/gallery/pic4.jpg',},
+    {cover: 'images/gallery/pic5.jpg',},
+    {cover: 'images/gallery/pic6.jpg',},
+    {cover: 'images/gallery/pic7.jpg',},
+    {cover: 'images/gallery/pic8.jpg',},
+    {cover: 'images/gallery/pic13.jpg',},
+    {cover: 'images/gallery/pic14.jpg',}
+    ];
+}]);
