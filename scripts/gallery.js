16cd183e0e57fbd75fb35b7ae93185264a646d41
jaga <jagawebdev@gmail.com>
2016-08-23 03:51:20 +0000

    fancybox

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -2,8 +2,6 @@
 /*global angular */
 
 $(document).ready(function() {
-  
-  
   $('.hamburger').click( function(event){
         event.stopPropagation();
         $('.drop-menu').toggle();
@@ -16,26 +14,7 @@ $(document).ready(function() {
     $(".list-image").mouseenter(function() {
       $(".list-image").siblings().addClass("fade");
     });
+    
+    $(".fancybox").fancybox();
 
 });
-
-
-var app = angular.module("myApp", []);
-
-app.controller("MainController", 
-['$scope', function($scope) {
-  $scope.products = [
-    {cover: 'images/gallery/pic9.jpg',},
-    {cover: 'images/gallery/pic10.jpg',},
-    {cover: 'images/gallery/pic11.jpg',},
-    {cover: 'images/gallery/pic12.jpg',},
-    {cover: 'images/gallery/pic1.jpg',},
-    {cover: 'images/gallery/pic3.jpg',},
-    {cover: 'images/gallery/pic4.jpg',},
-    {cover: 'images/gallery/pic5.jpg',},
-    {cover: 'images/gallery/pic6.jpg',},
-    {cover: 'images/gallery/pic7.jpg',},
-    {cover: 'images/gallery/pic13.jpg',},
-    {cover: 'images/gallery/pic14.jpg',}
-    ];
-}]);
